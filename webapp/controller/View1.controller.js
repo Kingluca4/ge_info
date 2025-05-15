64f2b1c8cdec856041468b5c5250eef618301c94
Luca Gulotta <l.gulotta@tecnolife.com>
2025-05-16 01:29:54 +0200

    Fix Trade parade

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -18,7 +18,21 @@ sap.ui.define([
             onInit: function () {
                 let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.getRoute("RouteView1").attachPatternMatched(this.onRouteMatched, this);
-                
+
+                var that = this;
+
+                    // Attach listener for first user interaction (click/touch)
+                    document.addEventListener("click", function playMusicOnce() {
+                        var audio = document.getElementById("bgmusic");
+                        if (audio) {
+                        audio.play().catch(function (error) {
+                            console.warn("Autoplay failed:", error);
+                        });
+                        }
+
+                        // Remove the listener after first interaction
+                        document.removeEventListener("click", playMusicOnce);
+                    });
             },
 
             onRouteMatched: function(oEvent, targetName) { 
