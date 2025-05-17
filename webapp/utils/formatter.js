164f37c5d0914977dc3b2259dfcb2d21cd32b027
Luca Gulotta <l.gulotta@tecnolife.com>
2025-05-17 23:15:50 +0200

    Added Formatter + coins icon

--- webapp/utils/formatter.js
+++ webapp/utils/formatter.js
@@ -11,7 +11,24 @@ sap.ui.define([], function () {
                 // Provide a default image or an empty string if imageUrl is empty
                 return "";
             }
+        },
+
+        formatPrice: function (value) {
+        if (value == null || isNaN(value)) return "";
+        return Number(value).toLocaleString();
+        },
+
+        selectCoinIcon: function (value) {
+        const num = Number(value);
+        if (num > 1_000_000) {
+            return "icons/Coins_10000.png";
+        } else if (num > 100_000) {
+            return "icons/Coins_250.png";
+        } else {
+            return "icons/Coins_5.png";
         }
+        }
+
 
     };
 
